4370844ad89eec8981759523f042e8e98a9e0c61
bestorbust <bestorbust@gmail.com>
2025-01-10 19:25:47 +0530

    modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,31 +8,31 @@ import { Component,OnInit } from '@angular/core';
 })
 export class HomeComponent {
   texts: string[] = ['Priya Darshini', 'Web Developer'];
-  displayedText: string = 'Priya Darshini';
-  // displayedText: string = this.texts[0];
+  // displayedText: string = 'Priya Darshini';
+  displayedText: string = this.texts[0];
   currentIndex: number = 0;
 
-  // ngOnInit(): void {
-  //   this.startTextLoop();
-  // }
+  ngOnInit(): void {
+    this.startTextLoop();
+  }
 
-  // startTextLoop(): void {
-  //   setInterval(() => {
-  //     this.currentIndex = (this.currentIndex + 1) % this.texts.length;
-  //     this.displayedText = this.texts[this.currentIndex];
-  //   }, 2000); 
-  // }
-  // profilePath:string ='assets/images/profile.jpg';
-  // displayedText: string = 'Priyadarshini P';
-  // isPriyadarshini: boolean = true;
-  // ngOnInit() {
-  //   setInterval(() => {
-  //     this.isPriyadarshini = !this.isPriyadarshini;
-  //     this.displayedText = this.isPriyadarshini
-  //       ? 'Priyadarshini P'
-  //       : 'Web Developer';
-  //   }, 5000);
-  // }
+  startTextLoop(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.texts.length;
+      this.displayedText = this.texts[this.currentIndex];
+    }, 2000); 
+  }
+  profilePath:string ='assets/images/profile.jpg';
+  displayedText: string = 'Priyadarshini P';
+  isPriyadarshini: boolean = true;
+  ngOnInit() {
+    setInterval(() => {
+      this.isPriyadarshini = !this.isPriyadarshini;
+      this.displayedText = this.isPriyadarshini
+        ? 'Priyadarshini P'
+        : 'Web Developer';
+    }, 5000);
+  }
 
   // ngOnInit() {
   //   setInterval(() => {
