cc191af011ff6ee163effc2ce10fdb8a00e6b57b
priyadarshini_P <155546102+bestorbust@users.noreply.github.com>
2025-01-10 21:54:53 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,14 +18,6 @@ export async function netlifyAppEngineHandler(request: Request): Promise<Respons
   return result || new Response('Not found', { status: 404 })
 }
 
-import { CommonEngine } from '@angular/ssr/node'
-import { render } from '@netlify/angular-runtime/common-engine'
-
-const commonEngine = new CommonEngine()
-
-export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-  return await render(commonEngine)
-}
 
 let netlifyContext: any;
 
