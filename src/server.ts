3c4bff3de61d94d1c045ec43b605b9536fc8195f
bestorbust <bestorbust@gmail.com>
2025-01-10 19:14:08 +0530

    modified

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,25 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
+import { getContext } from '@netlify/angular-runtime/context';
+const angularAppEngine = new AngularAppEngine()
+
+export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
+  const context = getContext()
+
+  const result = await angularAppEngine.handle(request, context)
+  return result || new Response('Not found', { status: 404 })
+}
+
+import { CommonEngine } from '@angular/ssr/node'
+import { render } from '@netlify/angular-runtime/common-engine'
+
+const commonEngine = new CommonEngine()
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  return await render(commonEngine)
+}
 
 let netlifyContext: any;
 
